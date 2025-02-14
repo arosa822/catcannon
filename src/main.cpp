a9a30d58430c5b5b44490f42323515f4366aaf9d
Alex Rosa <arosa@redhat.com>
2025-02-14 16:56:38 -0500

    add motor control

--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,39 @@
 
 Servo myServo;
 
-const int pollingInterval = 250; //milliseconds
+const int pollingInterval = 100; //milliseconds
+const int maxDist = 40;
 
+// these are my sensors
 const int trigPin_0 = 2;
 const int echoPin_0 = 3;
-
 const int trigPin_1 = 4;
 const int echoPin_1 = 5;
-
 const int trigPin_2 = 6;
 const int echoPin_2 = 7;
-
 const int trigPin_3 = 8;
 const int echoPin_3 = 9;
+const int servoPin  = 10;
 
+char dataBuff[100];
 long duration;
 int distance;
-int d0;
-int d1;
-int d2;
-int d3;
 
-char sensor_buffer[100];
-
+// sensor data stores
+typedef struct {
+  int d0,d1,d2,d3;
+} Sensors;
 
 
-
-void aimCannon(int);
+void aimCannon(Sensors data);
+void pollSensors(Sensors *data);
 int getDistance(int,int);
 
 void setup() {
   Serial.begin(9600);
 
   // servo
-  myServo.attach(9);
+  myServo.attach(10);
 
   // transducers
   pinMode(trigPin_0, OUTPUT);
@@ -53,20 +52,27 @@ void setup() {
 
 void loop() {
 
-  // calculate distances from the sensor array
-  d0 = getDistance(trigPin_0,echoPin_0);
-  delay(pollingInterval);
-  d1 = getDistance(trigPin_1,echoPin_1);
-  delay(pollingInterval);
-  d2 = getDistance(trigPin_2,echoPin_2);
-  delay(pollingInterval);
-  d3 = getDistance(trigPin_3,echoPin_3);
-  delay(pollingInterval);
+  Sensors data ={-1};
+  pollSensors(&data);
 
-  sprintf(sensor_buffer,"d0:%d, d1:%d, d2:%d, d3:%d", d0, d1, d2, d3);
+  sprintf(dataBuff,"%d, %d,%d,%d", data.d0, data.d1, data.d2, data.d3);
+  Serial.println(dataBuff);
 
-  Serial.println(sensor_buffer);
+  aimCannon(data);
 }
+
+void pollSensors(Sensors *data) {
+
+  //Sensors data = {0};
+  // calculate distances from the sensor array
+  data->d0 = getDistance(trigPin_0,echoPin_0);
+  delay(pollingInterval);
+  data->d1 = getDistance(trigPin_1,echoPin_1);
+  delay(pollingInterval);
+  data->d2 = getDistance(trigPin_2,echoPin_2);
+  delay(pollingInterval);
+  data->d3 = getDistance(trigPin_3,echoPin_3);
+};
 
 int getDistance(int trigPin, int echoPin ) {
 
@@ -79,14 +85,27 @@ int getDistance(int trigPin, int echoPin ) {
   duration = pulseIn(echoPin, HIGH);
   distance = duration * 0.034 / 2;
 
+  if (distance > maxDist) {
+    distance = -1;
+  };
+
   return distance;
 }
 
-void aimCannon(int pos) {
+void aimCannon(Sensors data) {
   // pos sweeps servo from 0 to 180 degrees
-  myServo.write(pos);
+  if (data.d0 > 0){
+    myServo.write(30);
+  } else if (data.d1>0 ){
+    myServo.write(60);
+  } else if (data.d2>0 ){
+    myServo.write(120);
+  } else if (data.d3>0 ){
+    myServo.write(150);
+  }
 
   /** 
+  kghgihvkhbrebilrcvubfbhugcli
   // Sweep from 0 to 180 degrees
   for (int pos = 0; pos <= 180; pos += 1) {
     myServo.write(pos);    // Move servo to 'pos'
@@ -99,9 +118,4 @@ void aimCannon(int pos) {
     delay(15);
   }
     **/
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
